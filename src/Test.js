730a632f50a21a9bd233f0b471fbd66ae9e9901f
Nikhil Thadani <nikhilthadani112256@gmail.com>
2022-04-26 20:59:12 +0530

    final

---
+++ src/Test.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { Typography } from "@mui/material";
+const Test = () => {
+  return (
+    <div>
+      <Typography variant="h5" sx={{ color: "red" }}>
+        Hello World
+      </Typography>
+    </div>
+  );
+};
+
+export default Test;
