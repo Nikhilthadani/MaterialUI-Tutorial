a34413b40210bdda3c873bda3f2799ac2830d791
Nikhil Thadani <nikhilthadani112256@gmail.com>
2022-05-05 19:55:01 +0530

    final

--- src/Test.js
+++ src/Test.js
@@ -1,117 +1,24 @@
-import React, { useState } from "react";
-import {
-  Button,
-  Checkbox,
-  FormControl,
-  FormControlLabel,
-  FormGroup,
-  FormLabel,
-  InputLabel,
-  MenuItem,
-  Radio,
-  RadioGroup,
-  Select,
-  TextField,
-} from "@mui/material";
+import React from "react";
+import { Box, Button, TextField, Typography } from "@mui/material";
 const Test = () => {
-  const [inputs, setInputs] = useState({
-    name: "",
-    email: "",
-    password: "",
-    subscribe: false,
-    age: undefined,
-    gender: "",
-  });
-  const handleChange = (e) => {
-    setInputs((prevState) => ({
-      ...prevState,
-      [e.target.name]: e.target.value,
-    }));
-  };
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    console.log(inputs);
-  };
   return (
-    <div>
-      <form
-        style={{ display: "flex", flexDirection: "column" }}
-        onSubmit={handleSubmit}
-      >
-        <TextField
-          name="name"
-          value={inputs.name}
-          onChange={handleChange}
-          type={"text"}
-          sx={{ margin: 3 }}
-          placeholder="Name"
-          variant="outlined"
-        />
-        <TextField
-          name="email"
-          value={inputs.email}
-          onChange={handleChange}
-          type={"email"}
-          sx={{ margin: 3 }}
-          placeholder="Email"
-          variant="standard"
-        />
-        <TextField
-          name="password"
-          value={inputs.password}
-          onChange={handleChange}
-          type={"password"}
-          sx={{ margin: 3 }}
-          placeholder="Password"
-          variant="filled"
-        />
+    <Box
+      display={"flex"}
+      flexDirection={"column"}
+      margin={10}
+      border={10}
+      borderColor={"red"}
+      borderRadius={10}
+      padding={10}
+    >
+      <Box>
+        {" "}
+        <Typography>This is inside Box</Typography>
+      </Box>
 
-        <FormGroup>
-          <FormControlLabel
-            control={
-              <Checkbox
-                onChange={() =>
-                  setInputs((prev) => ({
-                    ...prev,
-                    subscribe: !inputs.subscribe,
-                  }))
-                }
-              />
-            }
-            label="Subscribe To Newsletter"
-          />
-        </FormGroup>
-
-        <FormControl fullWidth>
-          <InputLabel>Age</InputLabel>
-          <Select
-            name="age"
-            value={inputs.age}
-            label="Age"
-            onChange={handleChange}
-          >
-            <MenuItem value={10}>Ten</MenuItem>
-            <MenuItem value={20}>Twenty</MenuItem>
-            <MenuItem value={30}>Thirty</MenuItem>
-          </Select>
-        </FormControl>
-
-        <FormControl>
-          <FormLabel>Gender</FormLabel>
-          <RadioGroup onChange={handleChange} name="gender">
-            <FormControlLabel
-              value="female"
-              control={<Radio />}
-              label="Female"
-            />
-            <FormControlLabel value="male" control={<Radio />} label="Male" />
-            <FormControlLabel value="other" control={<Radio />} label="Other" />
-          </RadioGroup>
-        </FormControl>
-
-        <Button type="submit">Submit</Button>
-      </form>
-    </div>
+      <Button>Click Me</Button>
+      <TextField />
+    </Box>
   );
 };
 
