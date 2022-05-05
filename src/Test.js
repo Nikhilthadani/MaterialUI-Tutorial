d237d54110bd37d4f15e717763fa6649c170e266
Nikhil Thadani <nikhilthadani112256@gmail.com>
2022-05-05 09:37:22 +0530

    final

--- src/Test.js
+++ src/Test.js
@@ -1,30 +1,53 @@
 import React, { useState } from "react";
-import { TextField, Typography } from "@mui/material";
+import { Button, TextField } from "@mui/material";
 const Test = () => {
-  const [name, setName] = useState("");
+  const [inputs, setInputs] = useState({
+    name: "",
+    email: "",
+    password: "",
+  });
+  const handleChange = (e) => {
+    setInputs((prevState) => ({
+      ...prevState,
+      [e.target.name]: e.target.value,
+    }));
+  };
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(inputs);
+  };
   return (
     <div>
-      <TextField
-        value={name}
-        onChange={(e) => setName(e.target.value)}
-        type={"text"}
-        sx={{ margin: 3 }}
-        placeholder="Name"
-        variant="outlined"
-      />
-      <TextField
-        type={"email"}
-        sx={{ margin: 3 }}
-        placeholder="Email"
-        variant="standard"
-      />
-      <TextField
-        type={"password"}
-        sx={{ margin: 3 }}
-        placeholder="Password"
-        variant="filled"
-      />
-      <Typography>{name}</Typography>
+      <form onSubmit={handleSubmit}>
+        <TextField
+          name="name"
+          value={inputs.name}
+          onChange={handleChange}
+          type={"text"}
+          sx={{ margin: 3 }}
+          placeholder="Name"
+          variant="outlined"
+        />
+        <TextField
+          name="email"
+          value={inputs.email}
+          onChange={handleChange}
+          type={"email"}
+          sx={{ margin: 3 }}
+          placeholder="Email"
+          variant="standard"
+        />
+        <TextField
+          name="password"
+          value={inputs.password}
+          onChange={handleChange}
+          type={"password"}
+          sx={{ margin: 3 }}
+          placeholder="Password"
+          variant="filled"
+        />
+        <Button type="submit">Submit</Button>
+      </form>
     </div>
   );
 };
