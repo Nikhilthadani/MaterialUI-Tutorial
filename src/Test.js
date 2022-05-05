8c35816d1c0a41673ae5b350cc3b0e542208dc9c
Nikhil Thadani <nikhilthadani112256@gmail.com>
2022-05-05 18:43:33 +0530

    all form elements

--- src/Test.js
+++ src/Test.js
@@ -1,10 +1,26 @@
 import React, { useState } from "react";
-import { Button, TextField } from "@mui/material";
+import {
+  Button,
+  Checkbox,
+  FormControl,
+  FormControlLabel,
+  FormGroup,
+  FormLabel,
+  InputLabel,
+  MenuItem,
+  Radio,
+  RadioGroup,
+  Select,
+  TextField,
+} from "@mui/material";
 const Test = () => {
   const [inputs, setInputs] = useState({
     name: "",
     email: "",
     password: "",
+    subscribe: false,
+    age: undefined,
+    gender: "",
   });
   const handleChange = (e) => {
     setInputs((prevState) => ({
@@ -18,7 +34,10 @@ const Test = () => {
   };
   return (
     <div>
-      <form onSubmit={handleSubmit}>
+      <form
+        style={{ display: "flex", flexDirection: "column" }}
+        onSubmit={handleSubmit}
+      >
         <TextField
           name="name"
           value={inputs.name}
@@ -46,6 +65,50 @@ const Test = () => {
           placeholder="Password"
           variant="filled"
         />
+
+        <FormGroup>
+          <FormControlLabel
+            control={
+              <Checkbox
+                onChange={() =>
+                  setInputs((prev) => ({
+                    ...prev,
+                    subscribe: !inputs.subscribe,
+                  }))
+                }
+              />
+            }
+            label="Subscribe To Newsletter"
+          />
+        </FormGroup>
+
+        <FormControl fullWidth>
+          <InputLabel>Age</InputLabel>
+          <Select
+            name="age"
+            value={inputs.age}
+            label="Age"
+            onChange={handleChange}
+          >
+            <MenuItem value={10}>Ten</MenuItem>
+            <MenuItem value={20}>Twenty</MenuItem>
+            <MenuItem value={30}>Thirty</MenuItem>
+          </Select>
+        </FormControl>
+
+        <FormControl>
+          <FormLabel>Gender</FormLabel>
+          <RadioGroup onChange={handleChange} name="gender">
+            <FormControlLabel
+              value="female"
+              control={<Radio />}
+              label="Female"
+            />
+            <FormControlLabel value="male" control={<Radio />} label="Male" />
+            <FormControlLabel value="other" control={<Radio />} label="Other" />
+          </RadioGroup>
+        </FormControl>
+
         <Button type="submit">Submit</Button>
       </form>
     </div>
