e2d019e7d3f942b3969139378fbf9b1d0f60a696
Nikhil Thadani <nikhilthadani112256@gmail.com>
2022-05-06 17:47:54 +0530

    final

--- src/Test.js
+++ src/Test.js
@@ -1,23 +1,43 @@
 import React, { useState } from "react";
-import { Tabs, Tab, AppBar, Toolbar } from "@mui/material";
+import {
+  Button,
+  Card,
+  CardActionArea,
+  CardActions,
+  CardContent,
+  CardMedia,
+  Typography,
+} from "@mui/material";
 const Test = () => {
-  const [value, setValue] = useState();
   return (
     <div>
-      <AppBar>
-        <Toolbar>
-          <Tabs
-            indicatorColor="secondary"
-            textColor="inherit"
-            value={value}
-            onChange={(e, val) => setValue(val)}
-          >
-            <Tab label="First" />
-            <Tab label="Second" />
-            <Tab label="Third" />
-          </Tabs>
-        </Toolbar>
-      </AppBar>
+      <Card sx={{ maxWidth: 500 }}>
+        <CardActionArea>
+          <CardMedia
+            component="img"
+            height="140"
+            image="https://media.istockphoto.com/photos/aerial-view-of-london-and-the-river-thames-picture-id637710754?k=20&m=637710754&s=612x612&w=0&h=FG9cz6qFrOTMr1ale8bfZvJ3OXn_EONPYAkVmZAbz44="
+            alt="green iguana"
+          />
+          <CardContent>
+            <Typography gutterBottom variant="h5" component="div">
+              Visit To London
+            </Typography>
+            <Typography variant="body2" color="text.secondary">
+              Lizards are a widespread group of squamate reptiles, with over
+              6,000 species, ranging across all continents except Antarctica
+            </Typography>
+          </CardContent>
+        </CardActionArea>
+        <CardActions>
+          <Button size="small" color="primary">
+            Share
+          </Button>
+          <Button size="small" color="primary">
+            Save
+          </Button>
+        </CardActions>
+      </Card>
     </div>
   );
 };
