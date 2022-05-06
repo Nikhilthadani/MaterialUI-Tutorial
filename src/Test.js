b5c8a1d8c1d67907bf7821367fbbc257e2cb986c
Nikhil Thadani <nikhilthadani112256@gmail.com>
2022-05-06 11:55:17 +0530

    final

--- src/Test.js
+++ src/Test.js
@@ -1,24 +1,29 @@
-import React from "react";
-import { Box, Button, TextField, Typography } from "@mui/material";
+import React, { useState } from "react";
+import { Button, Menu, MenuItem } from "@mui/material";
 const Test = () => {
-  return (
-    <Box
-      display={"flex"}
-      flexDirection={"column"}
-      margin={10}
-      border={10}
-      borderColor={"red"}
-      borderRadius={10}
-      padding={10}
-    >
-      <Box>
-        {" "}
-        <Typography>This is inside Box</Typography>
-      </Box>
+  const [anchorElm, setAnchorElm] = useState(null);
+  const [open, setOpen] = useState(false);
+  const handleClose = () => {
+    setAnchorElm(null);
+    setOpen(false);
+  };
 
-      <Button>Click Me</Button>
-      <TextField />
-    </Box>
+  const handleClick = (e) => {
+    setAnchorElm(e.currentTarget);
+    setOpen(true);
+  };
+  return (
+    <div>
+      <Button variant="contained" onClick={handleClick}>
+        Open Menu
+      </Button>
+      <Button onClick={handleClick}> Open Menu Again</Button>
+      <Menu anchorEl={anchorElm} open={open} onClose={handleClose}>
+        <MenuItem onClick={handleClose}>Profile</MenuItem>
+        <MenuItem onClick={handleClose}>Balance</MenuItem>
+        <MenuItem onClick={handleClose}>Logout</MenuItem>
+      </Menu>
+    </div>
   );
 };
 
