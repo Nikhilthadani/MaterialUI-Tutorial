9e9dba9afd3a46134bb12f25ae01e18f78c4aa12
Nikhil Thadani <nikhilthadani112256@gmail.com>
2022-05-06 19:31:52 +0530

    final

--- src/Test.js
+++ src/Test.js
@@ -6,25 +6,24 @@ import {
   DialogContent,
   DialogContentText,
   DialogTitle,
+  Modal,
+  Typography,
 } from "@mui/material";
+import { Box } from "@mui/system";
 const Test = () => {
   const [open, setOpen] = useState(false);
   return (
     <div>
-      <Button onClick={() => setOpen(true)}>Open Dialog</Button>
-      <Dialog open={open} onClose={() => setOpen(false)}>
-        <DialogTitle>This is my Dialog</DialogTitle>
-        <DialogContent>
-          <DialogContentText>
-            A dialog is a popup windoiw which can ask user something like an
-            information or confirmation
-          </DialogContentText>
-        </DialogContent>
-        <DialogActions>
-          <Button onClick={() => setOpen(false)}>Cancel</Button>
-          <Button onClick={() => setOpen(false)}>Agree</Button>
-        </DialogActions>
-      </Dialog>
+      <Button onClick={() => setOpen(true)}>Open Modal</Button>
+      <Modal open={open} onClose={() => setOpen(false)}>
+        <Box position="absolute" top="50%" left="50%">
+          <Typography>It is a Modal</Typography>
+
+          <Button variant="contained" onClick={() => setOpen(false)}>
+            Click Me
+          </Button>
+        </Box>
+      </Modal>
     </div>
   );
 };
