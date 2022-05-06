aac25c2dfba29f8d9740283fe1da1257fe2e37e0
Nikhil Thadani <nikhilthadani112256@gmail.com>
2022-05-06 18:49:53 +0530

    final

--- src/Test.js
+++ src/Test.js
@@ -1,43 +1,30 @@
 import React, { useState } from "react";
 import {
   Button,
-  Card,
-  CardActionArea,
-  CardActions,
-  CardContent,
-  CardMedia,
-  Typography,
+  Dialog,
+  DialogActions,
+  DialogContent,
+  DialogContentText,
+  DialogTitle,
 } from "@mui/material";
 const Test = () => {
+  const [open, setOpen] = useState(false);
   return (
     <div>
-      <Card sx={{ maxWidth: 500 }}>
-        <CardActionArea>
-          <CardMedia
-            component="img"
-            height="140"
-            image="https://media.istockphoto.com/photos/aerial-view-of-london-and-the-river-thames-picture-id637710754?k=20&m=637710754&s=612x612&w=0&h=FG9cz6qFrOTMr1ale8bfZvJ3OXn_EONPYAkVmZAbz44="
-            alt="green iguana"
-          />
-          <CardContent>
-            <Typography gutterBottom variant="h5" component="div">
-              Visit To London
-            </Typography>
-            <Typography variant="body2" color="text.secondary">
-              Lizards are a widespread group of squamate reptiles, with over
-              6,000 species, ranging across all continents except Antarctica
-            </Typography>
-          </CardContent>
-        </CardActionArea>
-        <CardActions>
-          <Button size="small" color="primary">
-            Share
-          </Button>
-          <Button size="small" color="primary">
-            Save
-          </Button>
-        </CardActions>
-      </Card>
+      <Button onClick={() => setOpen(true)}>Open Dialog</Button>
+      <Dialog open={open} onClose={() => setOpen(false)}>
+        <DialogTitle>This is my Dialog</DialogTitle>
+        <DialogContent>
+          <DialogContentText>
+            A dialog is a popup windoiw which can ask user something like an
+            information or confirmation
+          </DialogContentText>
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={() => setOpen(false)}>Cancel</Button>
+          <Button onClick={() => setOpen(false)}>Agree</Button>
+        </DialogActions>
+      </Dialog>
     </div>
   );
 };
