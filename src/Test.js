7f58c15ff4b94dafdd380dcd68d424b1ebe651ff
Nikhil Thadani <nikhilthadani112256@gmail.com>
2022-05-07 15:32:47 +0530

    final

--- src/Test.js
+++ src/Test.js
@@ -1,29 +1,27 @@
-import React, { useState } from "react";
-import {
-  Button,
-  Dialog,
-  DialogActions,
-  DialogContent,
-  DialogContentText,
-  DialogTitle,
-  Modal,
-  Typography,
-} from "@mui/material";
-import { Box } from "@mui/system";
+import React from "react";
+import { Container } from "@mui/material";
 const Test = () => {
-  const [open, setOpen] = useState(false);
   return (
     <div>
-      <Button onClick={() => setOpen(true)}>Open Modal</Button>
-      <Modal open={open} onClose={() => setOpen(false)}>
-        <Box position="absolute" top="50%" left="50%">
-          <Typography>It is a Modal</Typography>
-
-          <Button variant="contained" onClick={() => setOpen(false)}>
-            Click Me
-          </Button>
-        </Box>
-      </Modal>
+      <Container sx={{ background: "green" }} maxWidth="xs">
+        This is XS
+      </Container>
+      <br />
+      <Container sx={{ background: "green" }} maxWidth="sm">
+        This is SM
+      </Container>
+      <br />
+      <Container sx={{ background: "green" }} maxWidth="md">
+        This is MD
+      </Container>
+      <br />
+      <Container sx={{ background: "green" }} maxWidth="lg">
+        This is LG
+      </Container>
+      <br />
+      <Container sx={{ background: "green" }} maxWidth="xl">
+        This is XL
+      </Container>
     </div>
   );
 };
