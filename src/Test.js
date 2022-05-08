01e5c7ec260ce25ffe1e099008a86e64307886a5
Nikhil Thadani <nikhilthadani112256@gmail.com>
2022-05-08 14:25:44 +0530

    final

--- src/Test.js
+++ src/Test.js
@@ -1,27 +1,15 @@
 import React, { useState } from "react";
-import {
-  Button,
-  Drawer,
-  List,
-  ListItemButton,
-  ListItemText,
-} from "@mui/material";
+import { Alert } from "@mui/material";
 import { Box } from "@mui/system";
-const arrOptions = ["Profile", "Balance", "Logout"];
 const Test = () => {
-  const [open, setOpen] = useState(false);
+  const [show, setShow] = useState(true);
   return (
     <Box>
-      <Button onClick={() => setOpen(true)}>Open</Button>
-      <Drawer anchor="bottom" open={open} onClose={() => setOpen(false)}>
-        <List>
-          {arrOptions.map((elm) => (
-            <ListItemButton onClick={() => setOpen(false)}>
-              <ListItemText primary={elm} />
-            </ListItemButton>
-          ))}
-        </List>
-      </Drawer>
+      {show && (
+        <Alert onClose={() => setShow(false)} severity="info">
+          This is an Info Alert
+        </Alert>
+      )}
     </Box>
   );
 };
