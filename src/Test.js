c6739eec323981dd36f58ea0d3ef7e63bc4902a8
Nikhil Thadani <nikhilthadani112256@gmail.com>
2022-05-13 19:45:38 +0530

    final

--- src/Test.js
+++ src/Test.js
@@ -1,11 +1,18 @@
 import React, { useState } from "react";
-import { CircularProgress, LinearProgress } from "@mui/material";
+import { Rating, Typography } from "@mui/material";
 import { Box } from "@mui/system";
+
 const Test = () => {
+  const [value, setValue] = useState();
   return (
     <Box>
-      <CircularProgress color="secondary" variant="determinate" value={50} />
-      <LinearProgress color="inherit" variant="determinate" value={10} />
+      <Rating
+        precision={0.1}
+        size="large"
+        value={value}
+        onChange={(e, val) => setValue(val)}
+      />
+      <Typography>Rated {value !== undefined ? value : 0} Stars</Typography>
     </Box>
   );
 };
