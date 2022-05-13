bcdb66332f90a27df3bca52fd52ebd1621608b47
Nikhil Thadani <nikhilthadani112256@gmail.com>
2022-05-13 21:44:05 +0530

    final

--- src/Test.js
+++ src/Test.js
@@ -1,18 +1,72 @@
 import React, { useState } from "react";
-import { Rating, Typography } from "@mui/material";
+import { ImageList, ImageListItem, ImageListItemBar } from "@mui/material";
 import { Box } from "@mui/system";
+const itemData = [
+  {
+    img: "https://images.unsplash.com/photo-1551963831-b3b1ca40c98e",
+    title: "Breakfast",
+  },
+  {
+    img: "https://images.unsplash.com/photo-1551782450-a2132b4ba21d",
+    title: "Burger",
+  },
+  {
+    img: "https://images.unsplash.com/photo-1522770179533-24471fcdba45",
+    title: "Camera",
+  },
+  {
+    img: "https://images.unsplash.com/photo-1444418776041-9c7e33cc5a9c",
+    title: "Coffee",
+  },
+  {
+    img: "https://images.unsplash.com/photo-1533827432537-70133748f5c8",
+    title: "Hats",
+  },
+  {
+    img: "https://images.unsplash.com/photo-1558642452-9d2a7deb7f62",
+    title: "Honey",
+  },
+  {
+    img: "https://images.unsplash.com/photo-1516802273409-68526ee1bdd6",
+    title: "Basketball",
+  },
+  {
+    img: "https://images.unsplash.com/photo-1518756131217-31eb79b20e8f",
+    title: "Fern",
+  },
+  {
+    img: "https://images.unsplash.com/photo-1597645587822-e99fa5d45d25",
+    title: "Mushrooms",
+  },
+  {
+    img: "https://images.unsplash.com/photo-1567306301408-9b74779a11af",
+    title: "Tomato basil",
+  },
+  {
+    img: "https://images.unsplash.com/photo-1471357674240-e1a485acb3e1",
+    title: "Sea star",
+  },
+  {
+    img: "https://images.unsplash.com/photo-1589118949245-7d38baf380d6",
+    title: "Bike",
+  },
+];
 
 const Test = () => {
-  const [value, setValue] = useState();
   return (
     <Box>
-      <Rating
-        precision={0.1}
-        size="large"
-        value={value}
-        onChange={(e, val) => setValue(val)}
-      />
-      <Typography>Rated {value !== undefined ? value : 0} Stars</Typography>
+      <ImageList variant="standard" sx={{ width: 400, height: 400 }} cols={3}>
+        {itemData.map((image, index) => (
+          <ImageListItem key={index}>
+            <img src={image.img} alt={image.title} loading="lazy" />
+            <ImageListItemBar
+              position="below"
+              subtitle={<span> Michael</span>}
+              title={image.title}
+            />
+          </ImageListItem>
+        ))}
+      </ImageList>
     </Box>
   );
 };
